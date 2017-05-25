e22db3f832c907cc80daa80c81d2e8ac3ad627d8
lxlph <xuanlinh.do@haw-hamburg.de>
2017-05-25 12:32:21 +0200

    sockjs und google api hinzugefügt; sockjs in index.html und app.js getestet

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,24 +1,63 @@
-const http = require('http');
-		path = require('path');
-
-//const handle = require('./handle')
-//const handle = require('./server/handle') mit Ordnerstruktur
-//const handle = require('.test')
-const express = require('express');
-
+var http = require('http');
+var	path = require('path');
+var express = require('express');
+var sockjs  = require('sockjs');
 const clientDirectory= path.join(__dirname, 'client');
 
-const app = express();
+// 1. Echo sockjs server
+var sockjs_opts = {sockjs_url: "http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js"};
 
+var sockjs_echo = sockjs.createServer(sockjs_opts);
+sockjs_echo.on('connection', function(conn) {
+    conn.on('data', function(message) {
+        conn.write(message);
+    });
+});
+
+// 2. Express server
+var app = express();
 app.use('/', express.static(clientDirectory));
+sockjs_echo.installHandlers(app, {prefix:'/echo'});
+
+// console.log(' [*] Listening on 0.0.0.0:3000' );
+// app.listen(3000, '0.0.0.0');
+
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/client/index.html');
+// });
 
 app.get('/', (req, res) => {
 	res.send('Hallo Node.js!!');
-	
+
 });
 
 const server = http.createServer(app);
-
 server.listen(3000, () => {
 	console.log('Server listen on port 3000.');
 });
+
+
+// const http = require('http');
+// 		path = require('path');
+//
+// //const handle = require('./handle')
+// //const handle = require('./server/handle') mit Ordnerstruktur
+// //const handle = require('.test')
+// const express = require('express');
+//
+// const clientDirectory= path.join(__dirname, 'client');
+//
+// const app = express();
+//
+// app.use('/', express.static(clientDirectory));
+//
+// app.get('/', (req, res) => {
+// 	res.send('Hallo Node.js!!');
+//
+// });
+//
+// const server = http.createServer(app);
+//
+// server.listen(3000, () => {
+// 	console.log('Server listen on port 3000.');
+// });
